6693bac2d67a933bc02f616ab0400a27c4191764
Goulve <goulven.thomas@outlook.fr>
2022-03-30 15:06:11 +0200

    add test attribut

--- main.py
+++ main.py
@@ -2,6 +2,24 @@
 class classe():
     def __init__(self):
         self.name="goulven"
+        self.couleur="red"
+
 
 def function(a,b):
-    ...
+    return a+b
+
+print("Hello World")
+
+# Commentaires
+
+abc=2
+
+li=[1,2,3,"abc"]
+
+for i in li:
+    print(li)
+
+a=1
+while a==1:
+    a=0
+    
